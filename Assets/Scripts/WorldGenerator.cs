2c4dd9f110ee6428587db8e08190bf6516e3d2f6
Ruslan Shvetsov <ruslansorokin234567@gmail.com>
2022-12-14 00:50:50 +0300

    generation

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -7,10 +7,15 @@ public class WorldGenerator : MonoBehaviour
     public int sideX;
     public int sideZ;
 
+    public float terDetail;
+    public float terHeight;
+    int seed;
+
     public GameObject[] blocks;
 
     void Start()
     {
+        seed = Random.Range(100000, 999999);
         Generate();
     }
 
@@ -20,7 +25,9 @@ public class WorldGenerator : MonoBehaviour
         {
             for (int z = 0; z < sideZ; z++)
             {
-                Instantiate(blocks[0], new Vector3(x, 0, z), Quaternion.identity);
+                int y = (int)(Mathf.PerlinNoise((x / 2 + seed) / terDetail, (z / 2 + seed) / terDetail) * terHeight);
+                GameObject Grass = Instantiate(blocks[0], new Vector3(x, y, z), Quaternion.identity) as GameObject;
+                Grass.transform.SetParent(GameObject.FindGameObjectWithTag("Environment").transform);
             }
         }
     }
